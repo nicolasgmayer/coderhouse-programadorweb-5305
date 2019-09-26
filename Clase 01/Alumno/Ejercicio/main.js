e7d792cb78ed60f37231b45b7ab1267172e80de7
nicolasgmayer <nicomayer64@gmail.com>
2019-09-26 19:53:51 -0300

    subida del desafio 1

---
+++ Clase 01/Alumno/Ejercicio/main.js
@@ -0,0 +1,29 @@
+//Pedir nombre y mostralo en consola
+
+let nombre1 = prompt("Ingrese un nombre: ");
+console.log("Tu nombre es: " + nombre1);
+
+
+//Suma
+
+let num1 = parseInt(prompt("Ingrese un numero"));
+console.log(num1);
+let num2 = 10;
+console.log(num2);
+console.log(num1 + num2);
+
+//Resta
+
+let num3 = parseInt(prompt("Ingrese un numero"));
+console.log(num3);
+let num4 = 5;
+console.log(num4);
+console.log(num3 - num4);
+
+//Suma de numeros pedidos por PROMPT
+
+let num5 = parseInt(prompt("Ingrese un numero: "));
+let num6 = parseInt(prompt("Ingrese numero a sumar: "));
+console.log(num5);
+console.log(num6);
+console.log(num5 + num6);
